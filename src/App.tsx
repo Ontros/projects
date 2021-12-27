b7579412ac130dff25c554fa7dc2ba3c093f95e9
Ontros <ontro512@gmail.com>
2021-12-27 11:46:53 +0100

    menrys

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import menrys from './menrys_buff.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={menrys} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Menrys je kokot!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtu.be/dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dukaz
         </a>
       </header>
     </div>
