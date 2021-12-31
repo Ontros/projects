0975f2e5c77e1ddcc492f6a877d34b05ef82208d
Ontros <ontro512@gmail.com>
2021-12-31 18:34:27 +0100

    Pre language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,52 +2,84 @@ import React, { useEffect, useState } from 'react';
 import './Colors.css'
 import './App.css';
 
+function Lang(lang: number, array: string | string[]) {
+  if (array.length === 1) {
+    return array
+  }
+  var ans = array[lang]
+  if (ans) {
+    console.log(array)
+    return ans
+  }
+  return "Language error"
+}
+interface Button {
+  text: string[];
+  redirect: string;
+}
+
+interface Project {
+  title: string;
+  description: string[];
+  buttons: Button[];
+}
+
+interface ProjectParams {
+  project: Project;
+  index: number;
+  lang: number;
+  visibleProjectNum: number;
+}
+
+function Project(params: ProjectParams) {
+  const { project, index, lang, visibleProjectNum } = params
+  return (
+    <div className={`project ${index < visibleProjectNum ? 'project-visible' : ''}`} key={index}>
+      <div className="project-title">{project.title}</div>
+      <div className="project-description">{Lang(lang, project.description)}</div>
+      {project.buttons.map((button) => {
+        return <a href={button.redirect}><div className="project-button"
+        >{Lang(lang, button.text)}</div></a>
+      })
+      }
+    </div>
+  )
+}
+
 function App() {
-  const projects = [
-    {
-      title: "Ontro Bot",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex velit dignissimos voluptas ab fugiat sit?",
-    },
-    {
-      title: "Ontro Bot",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex velit dignissimos voluptas ab fugiat sit?",
-    },
-    {
-      title: "Ontro Bot",
-      description: "Lorem, ipsum dolor sit fjkasdkfjadsh lkjsadlfkjalsdfkjlaskdjflaskjdflaskdjf lkjasdflkjasdflkjasdlfkjas lkjfsdalkjflsadkjflsakdjf lksajdflkjasdf lkjsad lfkjasdamet consectetur adipisicing elit. Ex velit dignissimos voluptas ab fugiat sit?",
-    },
-    {
-      title: "Ontro Bot",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex velit dignissimos voluptas ab fugiat sit?",
-    },
-    {
-      title: "Ontro Bot",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex velit dignissimos voluptas ab fugiat sit?",
-    },
-    {
-      title: "Ontro Bot",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex velit dignissimos voluptas ab fugiat sit?",
-    },
-  ]
+  var popupTimeout = 2000
+
+  //TODO: 1) CSS vice tlacitek 2) switchovani Langu 3) odebrat double kod na zobrazovani projektu
+
   const [projectDivs, setProjectDivs] = useState<JSX.Element[]>([])
   const [visibleProjectNum, setVisibleProjectNum] = useState(0)
-  //Pridavej do projectDivs postupne a rendruj to
+  const [lang, setLang] = useState(0) //0 = czech; 1 = english
+  const projects: Project[] = [
+    {
+      title: 'OntroBot',
+      description: ['My personal discord bot (currently offline)', 'Můj osobní discord bot (aktuálně offline)'],
+      buttons: [{
+        text: ['View source code', 'Zobrazit zdrojový kód'],
+        redirect: 'https://github.com/Ontros/OntroBot',
+      },
+      {
+        text: ['Invite to your server', 'Pozvat na server'],
+        redirect: 'https://discordapp.com/oauth2/authorize?client_id=610830662353682464&scope=bot&permissions=8'
+      }]
+    },
+  ]
+
   useEffect(() => {
     projects.forEach((project, index) => {
       setTimeout(() => {
         setVisibleProjectNum(index + 1)
-      }, index * 1000)
+      }, index * popupTimeout)
       appendToProjectDivs(
-        <div className={`project`} key={index}>
-          <div className="project-title">{project.title}</div>
-          <div className="project-description">{project.description}</div>
-          <div className="project-button">SHOW MORE</div>
-        </div>
+        <Project project={project} index={index} visibleProjectNum={visibleProjectNum} lang={lang} />
       )
     })
   }, [])
 
-  console.log(projectDivs)
   function appendToProjectDivs(appendix: JSX.Element) {
     var tempProjectDivs = projectDivs
     tempProjectDivs.push(
@@ -56,29 +88,48 @@ function App() {
     setProjectDivs(tempProjectDivs)
   }
 
-  console.log(visibleProjectNum)
   return (
     <div className="App">
       <div className="Header">
         <h1>Ontro</h1>
       </div>
+      <hr />
       <div className="projects">
         {
           projects.map((project, index) => {
-            // setTimeout(() => {
             return (
-              <div className={`project ${index < visibleProjectNum ? 'project-visible' : ''}`} key={index}>
-                <div className="project-title">{project.title}</div>
-                <div className="project-description">{project.description}</div>
-                <div className="project-button">SHOW MORE</div>
-              </div>
+              <Project project={project} index={index} visibleProjectNum={visibleProjectNum} lang={lang} />
             )
-            // }, index * 1000)
           })
         }
       </div>
-    </div >
+    </div>
   );
 }
 
 export default App;
+// <div className={`project`} key={index}>
+//   <div className="project-title">{project.title}</div>
+//   <div className="project-description">{Lang(lang, project.description)}</div>
+//   {project.buttons.map((button) => {
+//     return <div className="project-button" onClick={() => {
+//       window.location.href = button.redirect
+//     }}>{Lang(lang, button.text)}</div>
+//   })/* <div className="project-button" onClick={() => {
+//     window.location.href = project.redirect
+//   }}>{Lang(lang, project.buttonText)}</div> */}
+// </div>
+
+//     <div className={`project ${index < visibleProjectNum ? 'project-visible' : ''}`} key={index}>
+//       <div className="project-title">{project.title}</div>
+//       <div className="project-description">{Lang(lang, project.description)}</div>
+//       {project.buttons.map((button) => {
+  //         return <a href={button.redirect}><div className="project-button"
+  //         // onClick={() => {
+    //         //   window.location.href = button.redirect
+    //         // }}
+    //         >{Lang(lang, button.text)}</div></a>
+    //       })/* <div className="project-button" onClick={() => {
+      //   window.location.href = project.redirect
+    // }}>{Lang(lang, project.buttonText)}</div> */}
+    //     </div>
