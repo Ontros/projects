8c9cb9fb287d568e7f59c675c7f4d627e8620827
Ontros <ontro512@gmail.com>
2022-01-02 15:10:04 +0100

    Created Utils.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,8 @@ import FlagCZ from './cz.svg'
 import FlagGB from './gb.svg'
 //@ts-expect-error
 import detectBrowserLanguage from 'detect-browser-language'
+import { Lang, LanguageSelect } from './Utils'
 
-function Lang(lang: number, array: string[]): string {
-  if (array.length === 1) {
-    return array[0]
-  }
-
-  var ans = array[lang]
-  if (ans) {
-    return ans
-  }
-  return "Language error"
-}
 
 interface Button {
   text: string[];
@@ -51,33 +41,6 @@ function Project(params: ProjectParams) {
   )
 }
 
-interface LangProps {
-  lang: number;
-  setLang: (arg0: number) => void;
-}
-
-function LanguageSelect(props: LangProps) {
-  const { lang, setLang } = props
-
-  var languages = [{
-    name: 'English',
-    flag: FlagGB
-  },
-  {
-    name: 'Česky',
-    flag: FlagCZ
-  }
-  ]
-
-  return (
-    <div className="language-selection" onClick={() => {
-      setLang(lang < languages.length - 1 ? lang + 1 : 0)
-    }}>
-      <img src={languages[lang].flag} alt="Flag" className='flag' />
-      {languages[lang].name}
-    </div>
-  )
-}
 
 function App() {
   var popupTimeout = 200
